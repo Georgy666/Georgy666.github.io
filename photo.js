830e06436aabeb15e379e07675eca66d2b75f039
Georgy <titancast37@gmail.com>
2017-10-16 19:50:32 +0300

    blog 0.9.3 add inst for gallary

--- photo.js
+++ photo.js
@@ -1,31 +1,22 @@
 var tok = ' 	c0397e1ad5be4452b3625d8d20e543de', // я уже давал ссылку чуть выше
-   username = 'xenia_zefirka', // имя пользователя
-   kolichestvo = 4;
+    userid = 1362124742, // ID пользователя, можно выкопать в исходном HTML, можно использовать спец. сервисы либо просто смотрите следующий пример :)
+    kolichestvo = 4; // ну это понятно - сколько фоток хотим вывести
 
-$.ajax({ // первый ajax запрос возвратит нам ID пользователя
- url: 'https://api.instagram.com/v1/users/search',
- dataType: 'jsonp',
- type: 'GET',
- data: {access_token: tok, q: username}, // по сути это просто поиск пользователя по его имени
- success: function(result){
-   console.log(result);
-   $.ajax({
-     url: 'https://api.instagram.com/v1/users/' + result.data[0].id + '/media/recent', // указываем ID первого найденного
-     dataType: 'jsonp',
-     type: 'GET',
-     data: {access_token: tok, count: kolichestvo},
-     success: function(result2){
-       console.log(result2);
-       for(x in result2.data){
-         $('ul').append('<li><img src="'+result2.data[x].images.thumbnail.url+'"></li>');
-       }
-         },
-     error: function(result2){
-       console.log(result2);
-     }
-   });
- },
- error: function(result){
-   console.log(result);
- }
+$.ajax({
+	url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent',
+	dataType: 'jsonp',
+	type: 'GET',
+	data: {access_token: tok, count: kolichestvo}, // передаем параметры, которые мы указывали выше
+	success: function(result){
+ 		console.log(result);
+		for( x in result.data ){
+			$('ul').append('<li><img src="'+result.data[x].images.low_resolution.url+'"></li>'); // result.data[x].images.low_resolution.url - это URL картинки среднего разрешения, 306х306
+			// result.data[x].images.thumbnail.url - URL картинки 150х150
+			// result.data[x].images.standard_resolution.url - URL картинки 612х612
+			// result.data[x].link - URL страницы данного поста в Инстаграм
+		}
+	},
+	error: function(result){
+		console.log(result); // пишем в консоль об ошибках
+	}
 });
