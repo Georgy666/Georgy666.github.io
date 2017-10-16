76abe373de945d03b121d610795454ef69705c10
Georgy <titancast37@gmail.com>
2017-10-16 21:27:07 +0300

    blog 0.9.3 add inst for gallary

--- photo.js
+++ photo.js
@@ -10,7 +10,7 @@ $.ajax({
 	success: function(result){
  		console.log(result);
 		for( x in result.data ){
-			$('ul').append('<li><img src="'+result.data[x].images.low_resolution.url+'"></li>'); // result.data[x].images.low_resolution.url - это URL картинки среднего разрешения, 306х306
+			$('ul').append('<li><img src="'+result.data[x].images.low_resolution.url+'"></li>'); result.data[x].images.low_resolution.url;
 			// result.data[x].images.thumbnail.url - URL картинки 150х150
 			// result.data[x].images.standard_resolution.url - URL картинки 612х612
 			// result.data[x].link - URL страницы данного поста в Инстаграм
