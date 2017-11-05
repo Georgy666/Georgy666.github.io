f5081f8efa0cff1ffb648684d67778d8928d465c
Georgy <titancast37@gmail.com>
2017-11-06 01:11:00 +0300

    blog 0.9.9J JS-fix

--- photo.js
+++ photo.js
@@ -12,9 +12,6 @@ jQuery(function($) {
      		console.log(result);
     		for( x in result.data ){
     			$('ul').append('<li><img src="'+result.data[x].images.thumbnail.url+'"></li>');
-
-
-        document.write(x);
            // result.data[x].images.low_resolution.url - это URL картинки среднего разрешения, 306х306
     			// result.data[x].images.thumbnail.url - URL картинки 150х150
     			// result.data[x].images.standard_resolution.url - URL картинки 612х612
