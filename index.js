817400bb61426f3d5b440d07ba50cdb3d60bd062
Georgy666 <titancast37@gmail.com>
2018-11-15 20:32:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'B3GQ6zA6Big', start: 3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
